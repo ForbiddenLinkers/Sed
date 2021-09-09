de19bd49e515ecf70ad89440e37bbe7496679a7d
Developer <72003733+InternetAmethyst@users.noreply.github.com>
2021-09-09 19:05:01 +0530

    Telegram

---
+++ userbot/telegramConnection.py
@@ -0,0 +1,104 @@
+from telethon import TelegramClient
+from telethon.network.connection.tcpabridged import ConnectionTcpAbridged
+from telethon.sessions import StringSession
+from .config import *
+
+if Config.STRING_SESSION:
+    session = StringSession(str(Config.STRING_SESSION))
+else:
+    session = "TelegramUBot"
+
+try:
+    bot = TelegramClient(
+        session=session,
+        api_id=Config.APP_ID,
+        api_hash=Config.API_HASH,
+        connection=ConnectionTcpAbridged,
+        auto_reconnect=True,
+        connection_retries=None,
+    )
+except Exception as e:
+    print(f"STRING_SESSION - {e}")
+    sys.exit()
+
+
+if Config.SESSION_2:
+    session2 = StringSession(str(Config.SESSION_2))
+else:
+    session2 = "TelegramUbot2"
+
+try:
+    bot2 = TelegramClient(
+        session=session2,
+        api_id=Config.APP_ID,
+        api_hash=Config.API_HASH,
+        connection=ConnectionTcpAbridged,
+        auto_reconnect=True,
+        connection_retries=None,
+    )
+except:
+    bot2 = None
+
+
+if Config.SESSION_3:
+    session3 = StringSession(str(Config.SESSION_3))
+else:
+    session3 = "bot3"
+
+try:
+    bot3 = TelegramClient(
+        session=session3,
+        api_id=Config.APP_ID,
+        api_hash=Config.API_HASH,
+        connection=ConnectionTcpAbridged,
+        auto_reconnect=True,
+        connection_retries=None,
+    )
+except:
+    bot3 = None
+
+
+if Config.SESSION_4:
+    session4 = StringSession(str(Config.SESSION_4))
+else:
+    session4 = "hellbot4"
+
+try:
+    bot4 = TelegramClient(
+        session=session4,
+        api_id=Config.APP_ID,
+        api_hash=Config.API_HASH,
+        connection=ConnectionTcpAbridged,
+        auto_reconnect=True,
+        connection_retries=None,
+    )
+except:
+    bot4 = None
+
+
+if Config.SESSION_5:
+    session5 = StringSession(str(Config.SESSION_5))
+else:
+    session5 = "bot5"
+
+try:
+    bot5 = TelegramClient(
+        session=session5,
+        api_id=Config.APP_ID,
+        api_hash=Config.API_HASH,
+        connection=ConnectionTcpAbridged,
+        auto_reconnect=True,
+        connection_retries=None,
+    )
+except:
+    bot5 = None
+
+
+TeleGramUBot = TelegramClient(
+    session="TeleGramUBot",
+    api_id=Config.APP_ID,
+    api_hash=Config.API_HASH,
+    connection=ConnectionTcpAbridged,
+    auto_reconnect=True,
+    connection_retries=None,
+).start(bot_token=Config.BOT_TOKEN)
